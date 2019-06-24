90f140f3786b2846c16ea5f9fce0a6e7753d8733
Lucas Napoli <lucasnapoli@wattopoststudio.com>
2019-06-24 14:02:17 -0400

    Repository Class added

--- src/main/java/com/lucas/soap/Book.java
+++ src/main/java/com/lucas/soap/Book.java
@@ -4,4 +4,60 @@ import java.io.Serializable;
 
 public class Book implements Serializable {
 
+    Integer id;
+    String title;
+    String description;
+    String isbn;
+    String author;
+    String publisher;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void String (String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+
 }
